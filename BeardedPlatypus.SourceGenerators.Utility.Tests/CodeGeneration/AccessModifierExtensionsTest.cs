98557e7b1cd996d089b206d23876914b1329efec
BeardedPlatypus <mwtegelaers@gmail.com>
2022-05-29 08:42:43 +0200

    Add AccessModifier

---
+++ BeardedPlatypus.SourceGenerators.Utility.Tests/CodeGeneration/AccessModifierExtensionsTest.cs
@@ -0,0 +1,19 @@
+﻿using BeardedPlatypus.SourceGenerators.Utility.CodeGeneration;
+using NUnit.Framework;
+
+namespace BeardedPlatypus.SourceGenerators.Utility.Tests.CodeGeneration;
+
+[TestFixture]
+public class AccessModifierExtensionsTest
+{
+    [Test]
+    [TestCase(AccessModifier.Public, "public", TestName = "public")]
+    [TestCase(AccessModifier.Internal, "internal", TestName = "internal")]
+    [TestCase(AccessModifier.Protected, "protected", TestName = "protected")]
+    [TestCase(AccessModifier.Private, "private", TestName = "private")]
+    public void ToString_ExpectedResults(AccessModifier modifier, string expectedResult)
+    {
+        var result = modifier.ToSyntaxString();
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
