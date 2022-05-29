02d8ecedbdec98884e8ed8d375de5d4e1a10f490
BeardedPlatypus <mwtegelaers@gmail.com>
2022-05-29 08:29:26 +0200

    Add Param

---
+++ BeardedPlatypus.SourceGenerators.Utility.Tests/CodeGeneration/ParamTest.cs
@@ -0,0 +1,158 @@
+﻿using BeardedPlatypus.SourceGenerators.Utility.CodeGeneration;
+using Microsoft.CodeAnalysis;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BeardedPlatypus.SourceGenerators.Utility.Tests.CodeGeneration;
+
+[TestFixture]
+public class ParamTest
+{
+    [Test]
+    public void Constructor_ExpectedResults()
+    {
+        const string paramName = "testData";
+        const string paramTypeName = nameof(TestCaseData);
+        const string paramDoc = "Some test-data.";
+
+        var param = new Param(paramName, paramTypeName, paramDoc);
+
+        Assert.That(param.TypeName, Is.EqualTo(paramTypeName));
+        Assert.That(param.Name, Is.EqualTo(paramName));
+        Assert.That(param.Doc, Is.EqualTo(paramDoc));
+    }
+
+    [Test]
+    public void Constructor_NameNull_ThrowsArgumentNullException()
+    {
+        void Call() => new Param(null, nameof(TestCaseData), null);
+        Assert.That(Call, Throws.ArgumentNullException
+                                       .With.Property(nameof(ArgumentNullException.ParamName))
+                                       .EqualTo("name"));
+    }
+
+    [Test]
+    public void Constructor_TypeNameNull_ThrowsArgumentNullException()
+    {
+        void Call() => new Param("testData", null, null);
+        Assert.That(Call, Throws.ArgumentNullException
+                                       .With.Property(nameof(ArgumentNullException.ParamName))
+                                       .EqualTo("typeName"));
+    }
+
+    [Test]
+    public void Constructor_DocNull_ExpectedResults()
+    {
+        var param = new Param("testData", nameof(TestCaseData), null);
+
+        Assert.That(param.Doc, Is.Null);
+    }
+
+    [Test]
+    [TestCase("<param name=\"name\">Some test-data</param>", "Some test-data")]
+    [TestCase("<param name=\"name\" >Some test-data</param >", "Some test-data")]
+    public void Constructor_ParamDocWithXml_IsStrippedOfXmlTags(string paramDoc, string expectedDocValue)
+    {
+        var param = new Param("name", nameof(TestCaseData), paramDoc);
+
+        Assert.That(param.Doc, Is.EqualTo(expectedDocValue));
+    }
+
+    private static IEnumerable<TestCaseData> NamedTypeSymbolConstructorData()
+    {
+        string name = nameof(TestCaseData);
+        string qualifiedName = typeof(TestCaseData).FullName;
+
+        var symbol = Substitute.For<INamedTypeSymbol>();
+        symbol.Name.Returns(name);
+        symbol.ToDisplayString().Returns(qualifiedName);
+
+        yield return new TestCaseData(symbol, true, qualifiedName).SetName("With qualified name.");
+        yield return new TestCaseData(symbol, false, name).SetName("Without qualified name.");
+    }
+
+    [Test]
+    [TestCaseSource(nameof(NamedTypeSymbolConstructorData))]
+    public void Constructor_TypeNamedTypeSymbol_ExpectedResults(INamedTypeSymbol typeSymbol, bool isFullyQualified, string expectedResult)
+    {
+        var param = new Param("name", typeSymbol, isFullyQualified, "Some test-data");
+        Assert.That(param.TypeName, Is.EqualTo(expectedResult));
+    }
+
+    private static IEnumerable<TestCaseData> ParameterSymbolConstructorData()
+    {
+        const string name = "testData";
+        const string docStr = "some test-data.";
+        const string docXml = $"<param name=\"{name}\">{docStr}</param>";
+
+        var symbol = Substitute.For<IParameterSymbol>();
+        symbol.Name.Returns(name);
+        symbol.GetDocumentationCommentXml()
+              .Returns(docXml);
+
+        const string typeName = nameof(TestCaseData);
+        string? qualifiedTypeName = typeof(TestCaseData).FullName;
+        var typeSymbol = Substitute.For<ITypeSymbol>();
+        typeSymbol.Name.Returns(typeName);
+        typeSymbol.ToDisplayString().Returns(qualifiedTypeName);
+
+        symbol.Type.Returns(typeSymbol);
+
+        yield return new TestCaseData(symbol, true, name, qualifiedTypeName, docStr);
+        yield return new TestCaseData(symbol, false, name, typeName, docStr);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(ParameterSymbolConstructorData))]
+    public void Constructor_FromParameterSymbol_ExpectedResults(IParameterSymbol parameterSymbol, 
+                                                                bool isFullyQualified,
+                                                                string expectedName,
+                                                                string expectedTypeName,
+                                                                string expectedDocs)
+    {
+        var param = new Param(parameterSymbol, isFullyQualified);
+
+        Assert.That(param.Name, Is.EqualTo(expectedName));
+        Assert.That(param.TypeName, Is.EqualTo(expectedTypeName));
+        Assert.That(param.Doc, Is.EqualTo(expectedDocs));
+    }
+
+    private static IEnumerable<TestCaseData> GenerateDocStringData()
+    {
+        const string name = "someName";
+        const string docStr = "some test-data.";
+        const string docXml = $"<param name=\"{name}\">{docStr}</param>";
+
+        yield return new TestCaseData(name, null, null)
+            .SetName("No documentation.");
+        yield return new TestCaseData(name, docStr, docXml)
+            .SetName("With doc string.");
+        yield return new TestCaseData(name, docXml, docXml)
+            .SetName("With doc xml.");
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GenerateDocStringData))]
+    public void GenerateDocString_ExpectedResults(string name, string doc, string expectedResult)
+    {
+        var param = new Param(name, nameof(TestCaseData), doc);
+
+        var result = param.GenerateDocString();
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void GenerateParamString_ExpectedResults()
+    {
+        const string paramName = "testData";
+        const string paramTypeName = nameof(TestCaseData);
+
+        var param = new Param(paramName, paramTypeName, null);
+
+        var result = param.GenerateParamString();
+
+        const string expectedResult = $"{paramTypeName} {paramName}";
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ BeardedPlatypus.SourceGenerators.Utility/CodeGeneration/Param.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using BeardedPlatypus.SourceGenerators.Utility.Internal;
+using Microsoft.CodeAnalysis;
+
+namespace BeardedPlatypus.SourceGenerators.Utility.CodeGeneration
+{
+    /// <summary>
+    /// <see cref="Param"/> describes a single parameter used
+    /// in the generation of code.
+    /// </summary>
+    public sealed class Param
+    {
+        // TODO: Add additional validation here?
+        /// <summary>
+        /// Creates a new <see cref="Param"/> with the given parameters.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="typeName">The type of the parameter.</param>
+        /// <param name="doc">The doc-string of the parameter</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="name"/> or <paramref name="typeName"/> are <c>null</c>.
+        /// </exception>
+        public Param(string name, string typeName, string doc = null)
+        {
+            Name = name.EnsureNotNull(nameof(name));
+            TypeName = typeName.EnsureNotNull(nameof(typeName));
+            Doc = ExtractDocString(doc);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Param"/> with the given parameters.
+        /// </summary>
+        /// <param name="name">The name of this parameter.</param>
+        /// <param name="typeSymbol">The type symbol specifying the type of this parameter.</param>
+        /// <param name="isFullyQualified">Whether the type name should be fully qualified.</param>
+        /// <param name="doc">The doc-string of the parameter.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="name"/> or <paramref name="typeSymbol"/> are <c>null</c>.
+        /// </exception>
+        public Param(string name, INamedTypeSymbol typeSymbol, bool isFullyQualified, string doc) :
+            this(name, ToTypeName(typeSymbol, isFullyQualified), doc) { }
+
+        /// <summary>
+        /// Creates a new <see cref="Param"/> from the given <paramref name="parameterSymbol"/>.
+        /// </summary>
+        /// <param name="parameterSymbol">The parameter symbol.</param>
+        /// <param name="isFullyQualified">Whether the parameter type should be fully qualified.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="parameterSymbol"/> is <c>null</c>.
+        /// </exception>
+        public Param(IParameterSymbol parameterSymbol, bool isFullyQualified = true) :
+            this(parameterSymbol?.Name, 
+                 ToTypeName(parameterSymbol?.Type, isFullyQualified),
+                 parameterSymbol?.GetDocumentationCommentXml()) { }
+
+        private static string ToTypeName(ISymbol symbol, bool isFullyQualified) =>
+            isFullyQualified ? symbol?.ToDisplayString() : symbol?.Name;
+
+        private static string ExtractDocString(string doc)
+        {
+            if (doc == null) return null;
+            if (!doc.StartsWith("<param name=\"")) return doc;
+
+            int docStrStart = doc.IndexOf(">", StringComparison.Ordinal) + 1;
+            int docStrEnd = doc.LastIndexOf("</", StringComparison.Ordinal - 1);
+
+            return doc.Substring(docStrStart, docStrEnd - docStrStart);
+        }
+
+        /// <summary>
+        /// Gets the parameter name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the name of the type of the parameter.
+        /// </summary>
+        public string TypeName { get; }
+
+        /// <summary>
+        /// Gets the documentation of this parameter.
+        /// </summary>
+        public string Doc { get; }
+
+        // TODO: Add options for wrapping the doc-string?
+        /// <summary>
+        /// Generate the doc-string of this <see cref="Param"/>.
+        /// </summary>
+        /// <returns>
+        /// The doc-string of this <see cref="Param"/>.
+        /// </returns>
+        public string GenerateDocString() =>
+            !(Doc is null) ? $"<param name=\"{Name}\">{Doc}</param>" : null;
+
+        /// <summary>
+        /// Generate the parameter string of this <see cref="Param"/>.
+        /// </summary>
+        /// <returns>
+        /// The parameter string: "{TypeName} {Name}".
+        /// </returns>
+        public string GenerateParamString() =>
+            $"{TypeName} {Name}";
+    }
+}
